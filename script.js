3b2fc778c27698d60c23e72b36b1ed4f72277082
ryan clayton <99753350+ryxncos@users.noreply.github.com>
2025-06-11 17:55:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerMenu = document.querySelector('.menuHamburguer');
-    const menu = document.querySelector('.menu'); 
+    const nav = document.querySelector('.nav');
+    const menuH = document.querySelector('.menuH');
 
-    hamburgerMenu.addEventListener('click', function() {
-        hamburgerMenu.classList.toggle('active');
-        menu.classList.toggle('active');
-    });
+    menuH.addEventListener('click', function(){
+        menuH.classList.toggle('active');
+        nav.classList.toggle('active');
 
-    // Opcional: Fechar o menu quando um link é clicado (útil em Single Page Applications)
-    menu.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', () => {
-            // Verifica se a largura da janela é menor ou igual ao breakpoint de mobile
-            if (window.innerWidth <= 768) {
-                hamburgerMenu.classList.remove('active');
-                menu.classList.remove('active');       
-            }
-        });
-    });
 });
+})
