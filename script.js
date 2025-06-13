e6cb4b279a618e23241c846a0ef683af0e25437f
ryan clayton <99753350+ryxncos@users.noreply.github.com>
2025-06-13 20:02:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const nav = document.querySelector('.nav');
     const menuH = document.querySelector('.menuH');
+    const main = document.querySelector('main');
 
     menuH.addEventListener('click', function(){
         menuH.classList.toggle('active');
         nav.classList.toggle('active');
 
 });
+
+    main.addEventListener('click', function(){
+        menuH.classList = ("menuH");
+        nav.classList = ("nav")
+});
+
 })
